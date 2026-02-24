bbe79855c78cf2501e64348741eea8238d5ccbca
Jawad Tech <jawadtechxd@gmail.com>
2026-02-24 17:51:06 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,3 @@
 module.exports = {
-  SESSION_ID: "",  // add session Id 
-  
-  PREFIX: ".", // prefix (e.g., ., /, !, *)
-  
-  TIMEZONE: "Asia/Karachi" // put your country timeZone....leave blank if u don't know.
+  SESSION_ID: "" // add you session id
 };
-
